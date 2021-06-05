7be4066f6dbe27c7d2787ef4bffe20896fac770b
eugena <eugena.mihailikova@gmail.com>
2021-06-05 20:13:13 +0500

    initial commit

---
+++ acesta_front/nuxt.config.js
@@ -0,0 +1,45 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Аналитический сервис для развития внутреннего туризма — аsecta',
+    htmlAttrs: {
+      lang: 'ru'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Аналитический сервис для развития внутреннего туризма на основе цифрового следа.' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '/css/bootstrap.min.css' }
+    ],
+    script: [
+      {src: '/js/bootstrap.bundle.min.js'}
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    // "bootstrap/dist/css/bootstrap.css"
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
