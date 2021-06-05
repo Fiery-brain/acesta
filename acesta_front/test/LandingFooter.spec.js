cb04deef4092fb627addc4dc6c5439bfb7b10491
eugena <eugena.mihailikova@gmail.com>
2021-06-05 21:03:55 +0500

    initial commit

--- acesta_front/test/LandingFooter.spec.js
+++ acesta_front/test/LandingFooter.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/LandingFooter.vue'
+import LandingFooter from '@/components/LandingFooter.vue'
 
 describe('LandingFooter', () => {
     test('is a Vue instance', () => {
-        const wrapper = mount(Logo)
+        const wrapper = mount(LandingFooter)
         expect(wrapper.vm).toBeTruthy()
     })
 })
--- acesta_front/test/LandingNavigation.spec.js
+++ acesta_front/test/LandingNavigation.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/LandingNavigation.vue'
+import LandingNavigation from '@/components/LandingNavigation.vue'
 
 describe('LandingNavigation', () => {
     test('is a Vue instance', () => {
-        const wrapper = mount(Logo)
+        const wrapper = mount(LandingNavigation)
         expect(wrapper.vm).toBeTruthy()
     })
 })
