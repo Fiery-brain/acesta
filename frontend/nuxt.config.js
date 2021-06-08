3b3379b73b29ecba96d827b1922db8a9adb001c5
eugena <eugena.mihailikova@gmail.com>
2021-06-08 10:12:35 +0500

    initial commit

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -32,7 +34,12 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/dotenv'
   ],
+
+  dotenv: {
+    systemvars: true
+  },
 
   loading: {
     continuous: true,
@@ -48,11 +55,84 @@ export default {
     '@nuxtjs/yandex-metrika'
   ],
 
+  axios: {
+    proxy: true,
+    credentials: true,
+    retry: {
+      retries: 3
+    }
+  },
+
+  proxy: {
+    '/api/': { target: process.env.API_URL, pathRewrite: {'^/api/': ''}, changeOrigin: true }
+  },
+
+  auth: {
+    redirect: {
+      login: '/login',
+      logout: '/',
+      callback: '/login',
+      home: '/'
+    },
+    strategies: {
+      local: {
+        scheme: 'refresh',
+          token: {
+            property: 'access_token',
+                maxAge: 1800,
+                global: true,
+          },
+          refreshToken: {
+            property: 'refresh_token',
+                data: 'refresh_token',
+                maxAge: 60 * 60 * 24 * 30
+          },
+          user: {
+            property: 'user',
+            // autoFetch: true
+          },
+          endpoints: {
+            login: { url: '/api/auth/login', method: 'post' },
+            refresh: { url: '/api/auth/refresh', method: 'post' },
+            user: { url: '/api/user', method: 'get' },
+            logout: { url: '/api/auth/logout', method: 'post' }
+
+          },
+          tokenType: 'Token',
+          tokenName: 'Authorization',
+          // autoLogout: false
+      },
+      google: {clientId: process.env.GOOGLE_CLIENT_ID},
+      facebook: {
+        endpoints: {
+          userInfo: 'https://graph.facebook.com/v6.0/me?fields=id,name,picture{url}'
+        },
+        clientId: process.env.FACEBOOK_CLIENT_ID,
+            scope: ['public_profile', 'email']
+      },
+      vk: {scheme: '~/schemes/vk', },
+      yandex: {scheme: '~/schemes/yandex', },
+    }
+  },
+
+  sentry: {
+    dsn: process.env.SENTRY_DSN,
+    maxBreadcrumbs: 50,
+    debug: true,
+  },
+
+  yandexMetrika: {
+    id: process.env.YANDEX_METRIKA_ID,
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: true
+  },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
 
   env: {
-    //test: '123'
   }
 }
