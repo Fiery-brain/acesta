341ad488eb77cc6415b5af2c8491863adc75dbd9
Eugena Mikhaylikova <eugena.mihailikova@gmail.com>
2021-06-14 18:10:35 +0500

    Update 0003_set_site_domain_and_name.py

--- backend/acesta/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ backend/acesta/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -13,7 +13,7 @@ def update_site_forward(apps, schema_editor):
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            "domain": "a-tourism.market",
+            "domain": "a-tourism.marketing",
             "name": "acesta",
         },
     )
@@ -23,7 +23,7 @@ def update_site_backward(apps, schema_editor):
     """Revert site domain and name to default."""
     Site = apps.get_model("sites", "Site")
     Site.objects.update_or_create(
-        id=settings.SITE_ID, defaults={"domain": "a-tourism.market", "name": "a-tourism.market"}
+        id=settings.SITE_ID, defaults={"domain": "a-tourism.marketing", "name": "a-tourism.marketing"}
     )
 
 
