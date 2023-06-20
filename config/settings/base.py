9b0d214fcd90f855c5992cab3b454a0fc419679c
eugena <eugena.mihailikova@gmail.com>
2023-06-20 10:09:20 +0500

    initial commit

--- config/settings/base.py
+++ config/settings/base.py
@@ -169,7 +169,6 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django_plotly_dash.middleware.BaseMiddleware",
     "django_plotly_dash.middleware.ExternalRedirectionMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
