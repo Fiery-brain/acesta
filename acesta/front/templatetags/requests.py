c5a8c9c7e0046e61826b6fbb9df456b741b0eed4
eugena <eugena.mihailikova@gmail.com>
2023-07-09 20:21:34 +0500

    feat(dev): user's requests (#291)

--- acesta/front/templatetags/requests.py
+++ acesta/front/templatetags/requests.py
@@ -1,13 +1,19 @@
 from django import template
 from django.conf import settings
+from django.contrib.auth.models import AnonymousUser
 
 from acesta.user.utils import get_request_form
 
 register = template.Library()
 
 
-@register.inclusion_tag("include/request_modal.html")
-def request_modal(request, user, subject):
+@register.inclusion_tag("include/request_modal.html", takes_context=True)
+def request_modal(context, subject):
+    request = context.get("request")
+    if request:
+        user = request.user
+    else:
+        user = AnonymousUser()
     return dict(
         REQUEST_CONSULTATION=settings.REQUEST_CONSULTATION,
         REQUEST_CHANNELS=dict(settings.REQUEST_CHANNELS_OUTSIDE),
