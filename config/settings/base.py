caf7472f7a3c05baa87d8847c6fa0bf9b8f26da3
eugena <eugena.mihailikova@gmail.com>
2024-04-13 16:15:34 +0500

    feat(dev): turning off google auth (#434)

--- config/settings/base.py
+++ config/settings/base.py
@@ -74,7 +74,6 @@ THIRD_PARTY_APPS = [
     "allauth.socialaccount.providers.vk",
     "allauth.socialaccount.providers.yandex",
     "acesta.socialaccount.providers.leaderid",
-    "allauth.socialaccount.providers.google",
 ]
 
 LOCAL_APPS = [
@@ -292,8 +291,7 @@ LOGGING = {
     "disable_existing_loggers": False,
     "formatters": {
         "verbose": {
-            "format": "%(levelname)s %(asctime)s %(module)s "
-            "%(process)d %(thread)d %(message)s"
+            "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
         }
     },
     "handlers": {
