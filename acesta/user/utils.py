337f1ee4af3e24f4bcd76bd8ff88fd7d9c0c14a7
eugena <eugena.mihailikova@gmail.com>
2024-11-05 10:57:41 +0500

    build(deps): update libs (#523)

--- acesta/user/utils.py
+++ acesta/user/utils.py
@@ -80,4 +80,4 @@ def send_message(message) -> None:
     :return: None
     """
     if not settings.TESTING:
-        return mail_admins(message, message)
+        return mail_admins(message.replace("\n", " "), message)
