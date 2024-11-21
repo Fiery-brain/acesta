557b997bfdb3414f90443e93a2afe1869e29fd83
eugena <eugena.mihailikova@gmail.com>
2024-11-21 14:55:56 +0500

    feat(dev): update date output (#535)

--- acesta/stats/helpers.py
+++ acesta/stats/helpers.py
@@ -1,8 +1,14 @@
+from datetime import datetime
+
 from django.core.cache import caches
 
 from acesta.geo.models import Sight
 from acesta.stats.models import AudienceRegions
 from acesta.stats.models import RegionRating
+
+DEFAULT_SIGHTS_UPDATE_DATE = datetime.fromisoformat("2023-01-01")
+DEFAULT_AUDITORY_UPDATE_DATE = datetime.fromisoformat("2023-01-01")
+DEFAULT_RATING_UPDATE_DATE = datetime.fromisoformat("2023-01-01")
 
 
 def get_sights_update_date():
@@ -13,8 +19,11 @@ def get_sights_update_date():
     cache = caches["db"]
     sights_update_date = cache.get("sights_update_date")
     if sights_update_date is None:
-        sights_update_date = Sight.pub.order_by("-modified").first().modified
-        cache.set("sights_update_date", sights_update_date, 60 * 60 * 24 * 7)
+        try:
+            sights_update_date = Sight.pub.order_by("-modified").first().modified
+            cache.set("sights_update_date", sights_update_date, 60 * 60 * 24 * 7)
+        except AttributeError:
+            sights_update_date = DEFAULT_SIGHTS_UPDATE_DATE
     return sights_update_date
 
 
@@ -26,10 +35,13 @@ def get_auditory_update_date():
     cache = caches["db"]
     auditory_update_date = cache.get("auditory_update_date")
     if auditory_update_date is None:
-        auditory_update_date = (
-            AudienceRegions.objects.order_by("-modified").first().modified
-        )
-        cache.set("auditory_update_date", auditory_update_date, 60 * 60 * 24 * 7)
+        try:
+            auditory_update_date = (
+                AudienceRegions.objects.order_by("-modified").first().modified
+            )
+            cache.set("auditory_update_date", auditory_update_date, 60 * 60 * 24 * 7)
+        except AttributeError:
+            auditory_update_date = DEFAULT_AUDITORY_UPDATE_DATE
     return auditory_update_date
 
 
@@ -41,6 +53,9 @@ def get_rating_update_date():
     cache = caches["db"]
     rating_update_date = cache.get("rating_update_date")
     if rating_update_date is None:
-        rating_update_date = RegionRating.objects.first().modified
-        cache.set("rating_update_date", rating_update_date, 60 * 60 * 24 * 7)
+        try:
+            rating_update_date = RegionRating.objects.first().modified
+            cache.set("rating_update_date", rating_update_date, 60 * 60 * 24 * 7)
+        except AttributeError:
+            rating_update_date = DEFAULT_AUDITORY_UPDATE_DATE
     return rating_update_date
