1c154ad0dfb0a09d57c8768f3ffb7b7299444329
eugena <eugena.mihailikova@gmail.com>
2025-04-08 10:59:45 +0500

    feat(dev): recommendations (#566)

--- acesta/stats/migrations/0006_auto_20230304_1818.py
+++ acesta/stats/migrations/0006_auto_20230304_1818.py
@@ -2,25 +2,29 @@
 
 import datetime
 from django.db import migrations, models
-from django.utils.timezone import utc
+
+# from django.utils.timezone import utc
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('stats', '0005_rename_updates_update'),
+        ("stats", "0005_rename_updates_update"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='update',
-            name='plan',
-            field=models.DateField(default=datetime.datetime(2023, 3, 4, 15, 18, 21, 697304, tzinfo=utc), verbose_name='Плановая дата следующего обновления'),
+            model_name="update",
+            name="plan",
+            field=models.DateField(
+                default=datetime.datetime(2023, 3, 4, 15, 18, 21, 697304),  # , tzinfo=utc
+                verbose_name="Плановая дата следующего обновления",
+            ),
             preserve_default=False,
         ),
         migrations.AlterField(
-            model_name='update',
-            name='date',
-            field=models.DateField(verbose_name='Дата обновления'),
+            model_name="update",
+            name="date",
+            field=models.DateField(verbose_name="Дата обновления"),
         ),
     ]
