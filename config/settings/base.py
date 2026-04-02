53995e1d59ddc7f5c2863ee3676c80232901cc59
eugena <eugena.mihailikova@gmail.com>
2026-04-02 11:11:18 +0500

    build(deps): Authorization Improvement (#434)

--- config/settings/base.py
+++ config/settings/base.py
@@ -294,43 +294,44 @@ DB_MUTEX_TTL_SECONDS = 9 * 60 * 60
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        },
+    },
     "filters": {
-        "require_debug_false": {
-            "()": "django.utils.log.RequireDebugFalse",
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
         },
     },
     "handlers": {
         "console": {
-            "level": "DEBUG",
+            "level": "INFO",
+            "filters": ["require_debug_true"],
             "class": "logging.StreamHandler",
-            "formatter": "verbose",
+            "formatter": "simple",
         },
         "mail_admins": {
             "level": "ERROR",
-            "filters": ["require_debug_false"],
             "class": "django.utils.log.AdminEmailHandler",
         },
     },
     "loggers": {
         "django": {
             "handlers": ["console"],
-            "level": "INFO",
+            "propagate": True,
         },
         "django.request": {
             "handlers": ["mail_admins"],
             "level": "ERROR",
-        },
-        "django.security": {
-            "handlers": ["mail_admins"],
-            "level": "ERROR",
+            "propagate": False,
         },
     },
-    "formatters": {
-        "verbose": {
-            "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
-        }
-    },
-    "root": {"level": "INFO", "handlers": ["console"]},
 }
 
 # DADATA
