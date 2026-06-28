48059a66cbb936cf82f672f7f7bc23e07aa9a091
eugena <eugena.mihailikova@gmail.com>
2026-06-28 14:52:28 +0500

    feat(dev): Cookie banner improvement (#666)

--- acesta/static/js/cookie.banner.js
+++ acesta/static/js/cookie.banner.js
@@ -2,13 +2,17 @@ $(document).ready(function() {
   const cookieBanner = $('#cookieBanner');
   const cookieBannerButton = $('#cookieBannerButton');
   const cookieBannerAccepted = 'acesta_cookie_banner_accepted';
+  const cookieBannerExpires = 90;
 
   if (!$.cookie(cookieBannerAccepted)) {
     cookieBanner.addClass('is-visible');
   }
 
   cookieBannerButton.on('click', function() {
-    $.cookie(cookieBannerAccepted, '1', { path: '/' });
+    $.cookie(cookieBannerAccepted, '1', {
+      expires: cookieBannerExpires,
+      path: '/',
+    });
     cookieBanner.removeClass('is-visible');
   });
 });
